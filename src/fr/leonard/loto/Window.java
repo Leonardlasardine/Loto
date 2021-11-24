2a6b8a8f216a89fb8952bf8c12eb3db70a786d28
Léonard <dranoel.lu@gmail.com>
2021-11-24 15:29:45 +0100

    windows with number of boards

--- src/fr/leonard/loto/Window.java
+++ src/fr/leonard/loto/Window.java
@@ -3,21 +3,39 @@ package fr.leonard.loto;
 import javax.swing.*;
 import java.awt.*;
 
-public class Window {
+public class Window extends JFrame {
 
-    public static void jFrame() {
+    public Window() throws HeadlessException {
         JFrame jFrame = new JFrame();
         jFrame.setTitle("Générateur de planches de loto " + Loto.version);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setLocationRelativeTo(null);
 
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BorderLayout());
 
-        Dimension minimumSize = new Dimension(200, 200);
+        Dimension minimumSize = new Dimension(300, 100);
         Dimension defaultSize = new Dimension(400, 400);
         jFrame.setMinimumSize(minimumSize);
         jFrame.setPreferredSize(defaultSize);
-        jFrame.pack();
 
+        JLabel nbPlanches = new JLabel("Nombre de planches");
+        JTextField repNbPlanches = new JTextField("100");
+
+        repNbPlanches.setPreferredSize(new Dimension(100, repNbPlanches.getFont().getSize() * 2));
+        repNbPlanches.setHorizontalAlignment(SwingConstants.RIGHT);
+        repNbPlanches.setCaretPosition(3);
+
+        JButton button = new JButton("Générer");
+        button.addActionListener(e -> System.out.println("Boutton"));
+
+        jPanel.add(nbPlanches, BorderLayout.WEST);
+        jPanel.add(repNbPlanches, BorderLayout.EAST);
+        jPanel.add(button, BorderLayout.SOUTH);
+
+        setContentPane(jPanel);
+        jFrame.add(jPanel);
+        jFrame.pack();
         jFrame.setVisible(true);
     }
 }
